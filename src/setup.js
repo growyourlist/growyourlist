7aed5665fb69b64d0b5c784dee4bcd7b3104a154
gyl-dev <gyl-dev@posteo.eu>
2020-02-25 22:56:51 +0100

    Get subcommand when run with npx

--- src/setup.js
+++ src/setup.js
@@ -1,9 +1,14 @@
 const gylInit = require('gyl-init');
 
-const command = process.argv[1];
+// Gets the subcommand to run when run globally.
+let command = process.argv[1];
+
+// Gets the subcommand to run when run with npx growyourlist <subcommand>
+if (command === 'growyourlist') {
+	command = process.argv[2];
+}
 if (!(command === 'setup' || command === 'destruct')) {
 	console.log('Must use growyourlist setup or growyourlist destruct');
-	console.log(command);
 	process.exit();
 }
 
